ae0fe99a2ec3edc266c627a902d986061ba3b2da
MfernandaJorge <mfjjorge@gmail.com>
2025-10-18 21:19:19 -0300

    Não teremos mais tela para cadastro de atendimento, a complexidade será definida no momento do agendamento

--- home-medcare-front/src/configs/fields/atendimentoFields.js
+++
@@ -1,5 +0,0 @@
-export const atendimentoFields = [
-  // { id:"id", name: "id", type: "number", placeholder: "Código" },
-  { id:"descricao", name: "descricao", type: "text", placeholder: "Descrição" },
-  { id:"complexidade", name: "complexidade", type: "number", placeholder: "Complexidade" },
-];
