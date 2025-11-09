e093beed51d55aa79bc98e61d4f612ebdc527aa6
MfernandaJorge <mfjjorge@gmail.com>
2025-11-09 17:11:54 -0300

    Incluindo logout no tratamento de erro das requisições.

--- home-medcare-front/src/services/api.js
+++ home-medcare-front/src/services/api.js
@@ -1,18 +1,37 @@
-import axios from 'axios';
+import axios from "axios";
+import { logout } from "../utils/logout/logout";
 
-const host = window.location.hostname; // Captura o host atual (pode ser localhost, IP ou domínio).
+const host = window.location.hostname;
 
 const api = axios.create({
-  baseURL: `http://${host}:8080/api`, // Monta dinamicamente a URL base da API.
+  baseURL: `http://${host}:8080/api`,
+  validateStatus: (status) => status >= 200 && status < 300,
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+// Configura token.
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// Tratamento de erros.
+api.interceptors.response.use(
+  (response) => {
+    // Sem error, apenas retorna a resposta.
+    return response;
+  },
+  (error) => {
+    if (error.response?.status === 403) {
+      logout();
+    }
+    return PromiseRejectionEvent;
   }
-
-  return config;
-});
+);
 
 export default api;
